1a63fd3d93f58a6c29274afc9991f63100677022
mohammad-haiderr-nadeem <haider.algolix@gmail.com>
2023-08-15 01:20:32 -0700

    first commit

---
+++ temp.js
@@ -0,0 +1,24 @@
+//JavaScript Tutorials
+// use camel case to declare variables in javascript
+
+//String Variable declaration syntax
+let myName = "haider";
+
+//Syntax to check the type of the variable
+console.log("type of the variable is:  ", typeof myName);
+
+//Integer Variable declaration
+let myNumber = 7;
+
+//syntax for checking the value
+console.log("value stored in the variable is: ", myNumber);
+
+//checking boolean values
+if (myNumber === 7) {
+  console.log("The number is equal"); //this will print if condition is true
+} else {
+  console.log("this number is not equal");
+}
+
+//Generates alert messages
+//alert("Hello World!!");
